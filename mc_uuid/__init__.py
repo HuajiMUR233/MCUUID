aa999b0c2bbb4cecc8c9f3ae71c9da7c081ee897
Huaji_MUR233 <huajimur@jvav.me>
2022-11-05 13:31:06 +0800

    Update __init__.py

--- mc_uuid/__init__.py
+++ mc_uuid/__init__.py
@@ -1,14 +1,21 @@
 from uuid import UUID
 import hashlib
-import json
 import re
 
 import requests
 
-__all__=["nameUUIDFromBytes","checkPlayerName","offlineUUID","onlineUUID"]
+__all__ = ["nameUUIDFromBytes", "checkPlayerName", "offlineUUID", "onlineUUID"]
 
 
 def nameUUIDFromBytes(name: bytes) -> UUID:
+    """
+    Static factory to retrieve a type 3 (name based) :class:`UUID` based on
+    the specified byte array.
+
+    :param name: A byte array to be used to construct a :class:`UUID`
+
+    :return: A :class:`UUID` generated from the specified array
+    """
     md5 = hashlib.md5()
     md5.update(name)
     md5Bytes = bytearray(md5.digest())
@@ -20,16 +27,38 @@ def nameUUIDFromBytes(name: bytes) -> UUID:
 
 
 def checkPlayerName(player: str) -> bool:
-    return (re.fullmatch(r"\w+",player) is not None) and len(player)<=16
+    """
+    Check if player name is legal
+
+    :param player: Player name
+
+    :return: Return `True` if the player name is legal
+    """
+    return (re.fullmatch(r"\w+", player) is not None) and len(player) <= 16
 
 
-def offlineUUID(player: str) -> UUID:
-    if not checkPlayerName(player):
+def offlineUUID(player: str, check_name: bool = True) -> UUID:
+    """
+    Generate a UUID based on the player name
+
+    :param player: Player name
+    :param check_name: Whether to check the legitimacy of the player name
+
+    :return: A :class:`UUID` generated from the player name
+    """
+    if check_name and not checkPlayerName(player):
         return None
     return nameUUIDFromBytes(("OfflinePlayer:" + player).encode())
 
 
 def onlineUUID(player: str) -> UUID:
+    """
+    Get player's online UUID from Mojang API
+
+    :param player: Player name
+
+    :return: A :class:`UUID` get from Mojang API
+    """
     if not checkPlayerName(player):
         return None
     response = requests.get("https://api.mojang.com/users/profiles/minecraft/" + player)
@@ -38,5 +67,5 @@ def onlineUUID(player: str) -> UUID:
     elif response.status_code != 200:
         raise Exception("Unknown error")
     result = response.json()
-    trimmedUUID=result['id']
+    trimmedUUID = result["id"]
     return UUID(trimmedUUID)
